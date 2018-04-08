71b673e63757b0188b31e65bef40f2343cf235bd
Stacy M <geekgirlbeta@gmail.com>
2018-04-08 15:04:29 -0400

    fixed initMap handler and altered html

--- js/sun.js
+++ js/sun.js
@@ -2,23 +2,40 @@ var coords = {
     lat: [],
     lng: [],
 }
+
 function initMap() {
-    var geocoder = new google.maps.Geocoder();  // Greates a Geocoder object to convert user input to lat and lng
-    $('#submitBTN').on('click', function () { 
+    var geocoder = new google.maps.Geocoder(); // Greates a Geocoder object to convert user input to lat and lng
+    $('#location-form').on('submit', function (event) {
         geocodeAddress(geocoder); // Uses geocoder, map objects to do coordinate conversion
+        event.preventDefault();
     })
 };
 // retreiving user input and set up of Geocoder
-function geocodeAddress(geocoder) { 
+function geocodeAddress(geocoder) {
     //adress can be zip code or any for of place name
-    var address = $("#locName").val();
-    geocoder.geocode({ 'address': address }, function (results, status) { 
-        if (status === 'OK') {            
+    var address = $("#search").val();
+    geocoder.geocode({
+        'address': address
+    }, function (results, status) {
+        if (status === 'OK') {
             // get lat and Lng and assign them to coords object
-            coords.lat = results[0].geometry.location.lat(); 
+            coords.lat = results[0].geometry.location.lat();
             coords.lng = results[0].geometry.location.lng();
         } else {
             console.log('Geocode was not successful for the following reason: ' + status);
         }
     });
-} 
+};
+
+
+/*
+$('input[name="location"]').click(function () {
+    alert('hii');
+});
+
+$("#search").keyup(function (e) {
+    if (e.which == 13) {
+        $('input[name="location"]').trigger('click');
+    }
+})
+*/
