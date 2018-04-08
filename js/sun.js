f635269c66f69ee07f1fbe07e1f740e326b7e6cf
Cemal Biryol <cbbiryol@cemalsmbook1774.attlocal.net>
2018-04-07 21:34:16 -0400

    java Script Fixed

--- js/sun.js
+++ js/sun.js
@@ -8,15 +8,17 @@ function initMap() {
         geocodeAddress(geocoder); // Uses geocoder, map objects to do coordinate conversion
     })
 };
-function geocodeAddress(geocoder) { // retreiving user input and set up of Geocoder
+// retreiving user input and set up of Geocoder
+function geocodeAddress(geocoder) { 
+    //adress can be zip code or any for of place name
     var address = $("#locName").val();
-    geocoder.geocode({ 'address': address }, function (results, status) { //adress can be zip code or any for of place name
+    geocoder.geocode({ 'address': address }, function (results, status) { 
         if (status === 'OK') {            
-            coords.lat = results[0].geometry.location.lat(); // get lat and Lng and assign them to coords object
-            coords.lng = results[0].geometry.location.lng();            
-            console.log(coords.lat + " " + coords.lng)
+            // get lat and Lng and assign them to coords object
+            coords.lat = results[0].geometry.location.lat(); 
+            coords.lng = results[0].geometry.location.lng();
         } else {
             console.log('Geocode was not successful for the following reason: ' + status);
         }
     });
-}
+} 
